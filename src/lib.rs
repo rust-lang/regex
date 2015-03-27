72a4ffe6ea6488d06dbde9dbe03b0a49c9340176
Andrew Gallant <jamslam@gmail.com>
2015-03-27 07:31:53 -0400

    Enable `core` for `pattern` feature.

--- src/lib.rs
+++ src/lib.rs
@@ -367,6 +367,7 @@
 
 #![deny(missing_docs)]
 #![cfg_attr(test, deny(warnings))]
+#![cfg_attr(feature = "pattern", feature(core))]
 #![doc(html_logo_url = "http://www.rust-lang.org/logos/rust-logo-128x128-blk-v2.png",
        html_favicon_url = "http://www.rust-lang.org/favicon.ico",
        html_root_url = "http://doc.rust-lang.org/regex/")]
