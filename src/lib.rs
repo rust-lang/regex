b0fe0254ce65837ee6d484050038700bff316e54
Andrew Gallant <jamslam@gmail.com>
2015-06-19 09:42:43 -0400

    rustup for nightly

--- src/lib.rs
+++ src/lib.rs
@@ -391,7 +391,7 @@
 
 #![deny(missing_docs)]
 #![cfg_attr(test, deny(warnings))]
-#![cfg_attr(feature = "pattern", feature(core))]
+#![cfg_attr(feature = "pattern", feature(pattern))]
 #![doc(html_logo_url = "http://www.rust-lang.org/logos/rust-logo-128x128-blk-v2.png",
        html_favicon_url = "http://www.rust-lang.org/favicon.ico",
        html_root_url = "http://doc.rust-lang.org/regex/")]
