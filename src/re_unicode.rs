d4680165afa4eedb3df00aae4b50d2f6584dfe96
Andrew Gallant <jamslam@gmail.com>
2016-05-06 22:05:32 -0400

    Drop the PartialEq and Eq impls on Regex.
    It is misleading to suggest that Regex implements equality, since
    equality is a well defined operation on regular expressions and this
    particular implementation doesn't correspond to that definition at all.
    
    Moreover, I suspect the actual use cases for such an impl are rather
    niche. A simple newtype+deref should resolve any such use cases.
    
    Fixes #178

--- src/re_unicode.rs
+++ src/re_unicode.rs
@@ -136,18 +136,6 @@ impl From<Exec> for Regex {
     }
 }
 
-/// Equality comparison is based on the original string. It is possible that
-/// different regular expressions have the same matching behavior, but are
-/// still compared unequal. For example, `\d+` and `\d\d*` match the same set
-/// of strings, but are not considered equal.
-impl PartialEq for Regex {
-    fn eq(&self, other: &Regex) -> bool {
-        self.as_str() == other.as_str()
-    }
-}
-
-impl Eq for Regex {}
-
 impl FromStr for Regex {
     type Err = Error;
 
