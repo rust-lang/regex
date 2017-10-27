918d4a0cddce47f9fd69cda2ad328f206597ba90
Ethan Pailes <ethanpailes@gmail.com>
2017-10-27 10:50:06 -0400

    search: skip dfa for anchored pats with captures
    The DFA can't produce captures, but is still faster than the Pike VM
    NFA, so the normal approach to finding capture groups is to look for
    the entire match with the DFA and then run the NFA on the substring
    of the input that matched. In cases where the regex in anchored, the
    match always starts at the beginning of the input, so there is never
    any point to trying the DFA first.
    
    The DFA can still be useful for rejecting inputs which are not in the
    language of the regular expression, but anchored regex with capture
    groups are most commonly used in a parsing context, so it seems like a
    fair trade-off.
    
    Fixes #348

--- bench/src/bench.rs
+++ bench/src/bench.rs
@@ -225,6 +225,41 @@ macro_rules! bench_find {
     }
 }
 
+// USAGE: bench_captures!(name, pattern, groups, haystack);
+//
+// CONTRACT:
+//   Given:
+//     ident, the desired benchmarking function name
+//     pattern : ::Regex, the regular expression to be executed
+//     groups : usize, the number of capture groups
+//     haystack : String, the string to search
+//   bench_captures will benchmark how fast re.captures() produces
+//   the capture groups in question.
+macro_rules! bench_captures {
+    ($name:ident, $pattern:expr, $count:expr, $haystack:expr) => {
+
+        #[cfg(feature = "re-rust")]
+        #[bench]
+        fn $name(b: &mut Bencher) {
+            use std::sync::Mutex;
+
+            lazy_static! {
+                static ref RE: Mutex<Regex> = Mutex::new($pattern);
+                static ref TEXT: Mutex<Text> = Mutex::new(text!($haystack));
+            };
+            let re = RE.lock().unwrap();
+            let text = TEXT.lock().unwrap();
+            b.bytes = text.len() as u64;
+            b.iter(|| {
+                match re.captures(&text) {
+                    None => assert!(false, "no captures"),
+                    Some(caps) => assert_eq!($count + 1, caps.len()),
+                }
+            });
+        }
+    }
+}
+
 mod ffi;
 mod misc;
 mod regexdna;
