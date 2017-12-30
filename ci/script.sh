b8f56f166e76853dda3f0521f11ae73d12788c64
Andrew Gallant <jamslam@gmail.com>
2017-12-30 09:37:18 -0500

    ci: limit testing on 1.12.0
    The latest update to rand requires a newer version of Rust. Since it's
    a dev dependency, we shouldn't need to do a semver bump when updating
    rand. However, CI needs to be told not to run tests. Instead, we merely
    check that we can build the crate and produce documentation.

--- ci/script.sh
+++ ci/script.sh
@@ -7,6 +7,16 @@ set -e
 # Builds the regex crate and runs tests.
 cargo build --verbose
 cargo doc --verbose
+
+# If we're testing on an older version of Rust, then only check that we
+# can build the crate. This is because the dev dependencies might be updated
+# more frequently, and therefore might require a newer version of Rust.
+#
+# This isn't ideal. It's a compromise.
+if [ "$TRAVIS_RUST_VERSION" = "1.12.0" ]; then
+  exit
+fi
+
 if [ "$TRAVIS_RUST_VERSION" = "nightly" ]; then
   cargo build --verbose --manifest-path regex-debug/Cargo.toml
   RUSTFLAGS="-C target-feature=+ssse3" cargo test --verbose --features 'simd-accel pattern' --jobs 4
