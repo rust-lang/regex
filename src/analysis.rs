b5b8f82fbde01d5a5b4689fdd44d246d785c93b3
Ethan Pailes <ethanpailes@gmail.com>
2018-09-23 06:25:28 -0400

    switch is_onepass to direct recursion instead of visitor.

--- src/analysis.rs
+++ src/analysis.rs
@@ -1,8 +1,7 @@
 use syntax::hir::{
     Hir, HirKind, Literal, ClassBytes, ClassBytesRange,
-    Class, Visitor, RepetitionRange, RepetitionKind
+    Class, RepetitionRange, RepetitionKind
 };
-use syntax::hir;
 use utf8_ranges::Utf8Sequences;
 
 /// True iff the given expression is onepass
@@ -49,119 +48,8 @@ use utf8_ranges::Utf8Sequences;
 /// we ever find a non-empty intersection, the regex
 /// is not onepass.
 pub fn is_onepass(expr: &Hir) -> bool {
-    hir::visit(expr, IsOnePassVisitor::new()).unwrap()
+    fset_of(expr).is_onepass
 }
-
-struct IsOnePassVisitor(bool);
-
-impl Visitor for IsOnePassVisitor {
-    type Output = bool;
-    type Err = ();
-
-    fn finish(self) -> Result<bool, ()> {
-        Ok(self.0)
-    }
-
-    fn visit_pre(&mut self, hir: &Hir) -> Result<(), ()> {
-        if !self.0 {
-            return Ok(())
-        }
-
-        match hir.kind() {
-            &HirKind::Concat(ref es) => self.check_concat(&es),
-            &HirKind::Alternation(ref es) => self.check_alternation(&es),
-            &HirKind::Repetition(ref rep) => {
-                if fset_of(&*rep.hir).is_empty() {
-                    self.0 = false;
-                }
-            }
-            &HirKind::Class(ref cls) => self.check_cls(cls),
-            _ => ()
-        }
-
-        Ok(())
-    }
-}
-
-impl IsOnePassVisitor {
-    fn new() -> Self {
-        IsOnePassVisitor(true)
-    }
-
-    fn check_concat(&mut self, es: &[Hir]) {
-        let mut empty_run = vec![];
-
-        for e in NestedConcat::new(es) {
-            let is_rep = match e.kind() {
-                &HirKind::Repetition(_) => true,
-                _ => false,
-            };
-
-            empty_run.push(e);
-            if !(accepts_empty(e) || is_rep) {
-                self.0 = self.0 && !fsets_clash(&empty_run);
-                empty_run.clear();
-            }
-        }
-
-        if empty_run.len() > 0 {
-            self.0 = self.0 && !fsets_clash(&empty_run);
-        }
-    }
-
-    fn check_alternation(&mut self, es: &[Hir]) {
-        self.0 = self.0 && !fsets_clash(&es.iter().collect::<Vec<_>>());
-    }
-
-    // Unicode classes are really just big alternatives from the byte
-    // oriented point of view.
-    //
-    // This function translates a unicode class into the 
-    // byte space and checks for intersecting first sets.
-    fn check_cls(&mut self, cls: &Class) {
-        match cls {
-            &Class::Unicode(ref ucls) => {
-                let mut seen_char: [bool; 256] = [false; 256];
-
-                for cr in ucls.iter() {
-                    for br in Utf8Sequences::new(cr.start(), cr.end()) {
-                        let first = br.as_slice()[0];
-                        for b in first.start..(first.end+1) {
-                            if seen_char[b as usize] {
-                                self.0 = false;
-                                return;
-                            }
-                            seen_char[b as usize] = true;
-                        }
-                    }
-                }
-            }
-            _ => {}
-        }
-    }
-
-}
-
-/// Check if a list of first sets is incompatible.
-///
-/// O(n^2), but n will usually be quite small.
-fn fsets_clash(es: &[&Hir]) -> bool {
-    for (i, e1) in es.iter().enumerate() {
-        for (j, e2) in es.iter().enumerate() {
-            if i != j {
-                let mut fset = fset_of(e1);
-                let fset2 = fset_of(e2);
-
-                fset.intersect(&fset2);
-                if ! fset.is_empty() {
-                    return true;
-                }
-            }
-        }
-    }
-    false
-}
-
 
 /// Compute the first set of a given regular expression.
 ///
@@ -182,9 +70,7 @@ fn fset_of(expr: &Hir) -> FirstSet {
         f
     }
 
-    // First compute the set of characters that might begin
-    // the expression (ignoring epsilon for now).
-    let mut f_char_set = match expr.kind() {
+    match expr.kind() {
         &HirKind::Empty => FirstSet::epsilon(),
         &HirKind::Literal(ref lit) => {
             match lit {
@@ -193,7 +79,7 @@ fn fset_of(expr: &Hir) -> FirstSet {
             }
         }
         &HirKind::Class(ref class) => {
-            match class {
+            let mut fset = match class {
                 &Class::Unicode(ref c) => {
                     // Get all the bytes which might begin this unicode
                     // class.
@@ -209,7 +95,10 @@ fn fset_of(expr: &Hir) -> FirstSet {
                 }
                 &Class::Bytes(ref b) =>
                     FirstSet::new(b.iter().map(|x| *x), false),
-            }
+            };
+
+            fset.is_onepass = class_is_onepass(class);
+            fset
         }
 
         // When an empty look (Anchor or WordBoundary) is at the start of
@@ -220,7 +109,25 @@ fn fset_of(expr: &Hir) -> FirstSet {
         // that such an emptylook could potentially match on any character.
         &HirKind::Anchor(_) | &HirKind::WordBoundary(_) => FirstSet::anychar(),
 
-        &HirKind::Repetition(ref rep) => fset_of(&rep.hir),
+        &HirKind::Repetition(ref rep) => {
+            let mut fset = fset_of(&rep.hir);
+
+            fset.accepts_empty = match rep.kind {
+                RepetitionKind::ZeroOrOne => true,
+                RepetitionKind::ZeroOrMore => true,
+                RepetitionKind::OneOrMore => fset.accepts_empty,
+                RepetitionKind::Range(ref range) => {
+                    match range {
+                        &RepetitionRange::Exactly(0)
+                        | &RepetitionRange::AtLeast(0)
+                        | &RepetitionRange::Bounded(0, _) => true,
+                        _ => fset.accepts_empty,
+                    }
+                }
+            };
+
+            fset
+        },
         &HirKind::Group(ref group) => fset_of(&group.hir),
 
         // The most involved case. We need to strip leading empty-looks
@@ -229,6 +136,10 @@ fn fset_of(expr: &Hir) -> FirstSet {
         // accept the empty string.
         &HirKind::Concat(ref es) => {
             let mut fset = FirstSet::empty();
+            let mut inner_fsets = Vec::with_capacity(es.len());
+            for e in es.iter() {
+                inner_fsets.push(fset_of(e));
+            }
             for (i, e) in es.iter().enumerate() {
                 match e.kind() {
                     &HirKind::Anchor(_) | &HirKind::WordBoundary(_) => {
@@ -236,13 +147,14 @@ fn fset_of(expr: &Hir) -> FirstSet {
                         // position have to be accounted for.
                         if i == es.len() - 1 {
                             fset.union(&FirstSet::anychar());
+                            fset.accepts_empty = false;
                         }
                     }
                     _ => {
-                        let inner_fset = fset_of(e);
-                        fset.union(&inner_fset);
+                        fset.union(&inner_fsets[i]);
 
-                        if !accepts_empty(e) {
+                        if !inner_fsets[i].accepts_empty {
+                            fset.accepts_empty = false;
                             // We can stop accumulating after we stop seeing
                             // first sets which contain epsilon.
                             break;
@@ -250,71 +162,94 @@ fn fset_of(expr: &Hir) -> FirstSet {
                     }
                 }
             }
+
+            fset.is_onepass = concat_is_onepass(es, &inner_fsets);
+
             fset
         }
         &HirKind::Alternation(ref es) => {
             let mut fset = FirstSet::empty();
-            for e in es {
-                fset.union(&fset_of(e));
+            let mut inner_fsets = Vec::with_capacity(es.len());
+            for (i, e) in es.iter().enumerate() {
+                inner_fsets.push(fset_of(e));
+                fset.union(&inner_fsets[i]);
             }
+
+            fset.is_onepass = !FirstSet::fsets_clash_value(&inner_fsets);
+
             fset
         }
-    };
-
-    f_char_set.accepts_empty = accepts_empty(expr);
-    f_char_set
+    }
 }
 
-fn accepts_empty(expr: &Hir) -> bool {
-    match expr.kind() {
-        &HirKind::Empty => true,
-        &HirKind::Literal(_) => false,
-        &HirKind::Class(_) => false,
+// Unicode classes are really just big alternatives from the byte
+// oriented point of view.
+//
+// This function translates a unicode class into the
+// byte space and checks for intersecting first sets.
+//
+// Byte classes are always onepass
+fn class_is_onepass(cls: &Class) -> bool {
+    match cls {
+        &Class::Unicode(ref ucls) => {
+            let mut seen_char: [bool; 256] = [false; 256];
 
-        // A naked empty look is a pretty weird thing because we
-        // normally strip them from the beginning of concatinations.
-        // We are just going to treat them like `.`
-        &HirKind::Anchor(_) | &HirKind::WordBoundary(_) => false,
-
-        &HirKind::Repetition(ref rep) => {
-            match rep.kind {
-                RepetitionKind::ZeroOrOne => true,
-                RepetitionKind::ZeroOrMore => true,
-                RepetitionKind::OneOrMore => accepts_empty(&rep.hir),
-                RepetitionKind::Range(ref range) => {
-                    match range {
-                        &RepetitionRange::Exactly(0)
-                        | &RepetitionRange::AtLeast(0)
-                        | &RepetitionRange::Bounded(0, _) => true,
-                        _ => accepts_empty(&rep.hir),
+            for cr in ucls.iter() {
+                for br in Utf8Sequences::new(cr.start(), cr.end()) {
+                    let first = br.as_slice()[0];
+                    for b in first.start..(first.end+1) {
+                        if seen_char[b as usize] {
+                            return false;
+                        }
+                        seen_char[b as usize] = true;
                     }
                 }
             }
         }
-
-        &HirKind::Group(ref group) => accepts_empty(&group.hir),
-
-        &HirKind::Concat(ref es) => {
-            let mut accepts: bool = true;
-            for e in es.iter() {
-                match e.kind() {
-                    &HirKind::Anchor(_) | &HirKind::WordBoundary(_) => {
-                        // Ignore any leading emptylooks.
-                    }
-                    _ => {
-                        accepts = accepts && accepts_empty(&e);
-                    }
-                }
-
-                if !accepts {
-                    break;
-                }
-            }
-            accepts
-        }
-
-        &HirKind::Alternation(ref es) => es.iter().any(accepts_empty)
+        _ => {}
     }
+
+    true
+}
+
+fn concat_is_onepass(es: &[Hir], inner_fsets: &[FirstSet]) -> bool {
+    let mut empty_run = vec![];
+
+    for (i, e) in NestedConcat::new(es).enumerate() {
+        match e.kind() {
+            &HirKind::Anchor(_) | &HirKind::WordBoundary(_) => {
+                if i < es.len() - 1 {
+                    continue;
+                }
+            }
+            _ => {} // FALLTHROUGH
+        }
+
+        if !inner_fsets[i].is_onepass {
+            return false;
+        }
+
+        let is_real_rep = match e.kind() {
+            &HirKind::Repetition(ref rep) => {
+                match rep.kind {
+                    RepetitionKind::Range(
+                        RepetitionRange::Exactly(_)) => false,
+                    _ => true,
+                }
+            },
+            _ => false,
+        };
+
+        empty_run.push(&inner_fsets[i]);
+        if !(inner_fsets[i].accepts_empty || is_real_rep) {
+            if FirstSet::fsets_clash_ref(&empty_run) {
+                return false;
+            }
+            empty_run.clear();
+        }
+    }
+
+    ! FirstSet::fsets_clash_ref(&empty_run)
 }
 
 /// The first byte of a unicode code point.
@@ -334,17 +269,43 @@ fn first_byte(c: char) -> u8 {
 /// string, so we add an extra flag to indicate if a FirstSet includes
 /// epsilon. Put in a more theoretical way all firstsets are subsets of
 /// SIGMA `union` { epsilon }.
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Clone)]
 struct FirstSet {
     bytes: ClassBytes,
-    pub accepts_empty: bool,
+    accepts_empty: bool,
+    is_onepass: bool,
 }
 
+/// A macro to define the fsets_clash associated functions,
+/// parameterized over the type of the inner slice. This lets
+/// us avoid allocating an extra vector when we check
+/// alternations for onepassness.
+macro_rules! def_fsets_clash {
+    ($fun_name:ident, $slice_inner:ty) => {
+        /// Check if a list of first sets is incompatible.
+        fn $fun_name(fsets: &[$slice_inner]) -> bool {
+            let mut seen_so_far = FirstSet::empty();
+
+            for fset in fsets.iter() {
+                let mut snapshot = seen_so_far.clone();
+                snapshot.intersect(&fset);
+                if ! snapshot.is_empty() {
+                    return true;
+                }
+
+                seen_so_far.union(&fset);
+            }
+
+            false
+        }
+    }
+}
 impl FirstSet {
     fn empty() -> Self {
         FirstSet {
             bytes: ClassBytes::empty(),
             accepts_empty: false,
+            is_onepass: true,
         }
     }
 
@@ -354,6 +315,7 @@ impl FirstSet {
         FirstSet {
             bytes: ClassBytes::new(ranges),
             accepts_empty: accepts_empty,
+            is_onepass: true,
         }
     }
 
@@ -367,6 +329,7 @@ impl FirstSet {
         FirstSet {
             bytes: ClassBytes::empty(),
             accepts_empty: true,
+            is_onepass: true,
         }
     }
 
@@ -387,6 +350,9 @@ impl FirstSet {
     fn is_empty(&self) -> bool {
         self.bytes.is_empty() && !self.accepts_empty
     }
+
+    def_fsets_clash!(fsets_clash_ref, &FirstSet);
+    def_fsets_clash!(fsets_clash_value, FirstSet);
 }
 
 /// An iterator over a concatenation of expressions which
@@ -420,7 +386,6 @@ impl<'a> Iterator for NestedConcat<'a> {
                 &HirKind::Concat(ref es) => {
                     self.0.push((es, 0));
                     continue;
-                    // self.next()
                 }
                 _ => return Some(&es[idx]),
             }
@@ -551,14 +516,23 @@ mod tests {
     }
 
     #[test]
-    fn fset_terminal_emptylook_all() {
+    fn fset_terminal_emptylook_all_1_() {
         let e = Parser::new().parse(r"a*\b").unwrap();
+        let mut fset = FirstSet::anychar();
+        fset.is_onepass = false;
 
-        let mut total_accept = FirstSet::anychar();
-        total_accept.accepts_empty = true;
-
-        assert_eq!(total_accept, fset_of(&e));
+        assert_eq!(fset, fset_of(&e), "\n\n{:?}\n\n", e);
     }
+
+    #[test]
+    fn fset_terminal_emptylook_all_2_() {
+        let e = Parser::new().parse(r"(a*)\b").unwrap();
+        let mut fset = FirstSet::anychar();
+        fset.is_onepass = false;
+
+        assert_eq!(fset, fset_of(&e), "\n\n{:?}\n\n", e);
+    }
+
 
     #[test]
     fn fset_empty_alt() {
@@ -572,45 +546,69 @@ mod tests {
     // onepass smoke tests
     //
 
-    // This test is pulled right from some of Russ Cox's
-    // comments on onepass regex.
-    //
-    // Note that Russ Cox's other example of a onepass regex
-    // (r"(\d+)-(\d+)") is actually not onepass for us because
-    // there is byte-level nondeterminism in the \d character
-    // class, and we care about things in the byte space rather
-    // than the character space. If you do a onepass engine at
-    // the character level, Cox's example is indeed onepass.
-    #[test]
-    fn is_onepass_smoke_test1() {
-        let e1 = Parser::new().parse(r"([^x]*)x(.*)").unwrap();
-        let e2 = Parser::new().parse(r"(.*)x(.*)").unwrap();
-
-        assert!(is_onepass(&e1));
-        assert!(!is_onepass(&e2));
+    macro_rules! test_onepass {
+        ($fun_name:ident, $re_str:expr) => {
+            #[test]
+            fn $fun_name() {
+                let e = Parser::new().parse($re_str).unwrap();
+                let fset = fset_of(&e);
+                assert!(fset.is_onepass, "fset={:?}", fset);
+            }
+        }
     }
 
-    #[test]
-    fn is_onepass_empty_alt() {
-        let e1 = Parser::new().parse(r"(a|())b").unwrap();
-        let e2 = Parser::new().parse(r"(a|())a").unwrap();
-
-        assert!(is_onepass(&e1));
-        assert!(!is_onepass(&e2));
-    }
-    
-    #[test]
-    fn is_onepass_rep() {
-        let e1 = Parser::new().parse(r"a+a").unwrap();
-        let e2 = Parser::new().parse(r"a*a").unwrap();
-
-        assert!(!is_onepass(&e1));
-        assert!(!is_onepass(&e2));
+    macro_rules! test_not_onepass {
+        ($fun_name:ident, $re_str:expr) => {
+            #[test]
+            fn $fun_name() {
+                let e = Parser::new().parse($re_str).unwrap();
+                let fset = fset_of(&e);
+                assert!(!fset.is_onepass, "fset={:?}", fset);
+            }
+        }
     }
 
-    #[test]
-    fn is_onepass_clash_in_middle_of_concat() {
-        let e = Parser::new().parse(r"ab?b").unwrap();
-        assert!(!is_onepass(&e));
-    }
+    test_onepass!(onepass_smoke_1_, r"[^x]x(.*)");
+    test_not_onepass!(onepass_smoke_2_, r"(.*)x(.*)");
+
+    test_not_onepass!(onepass_alt_1_, r"a|b|c|a|d");
+    test_not_onepass!(onepass_alt_2_, r"a|b|c|((m|a|x)|g)|d");
+    test_onepass!(onepass_alt_3_, r"a|b|c|x|d");
+    test_onepass!(onepass_alt_4_, r"a|b|c|((m|x)|g)|d");
+
+    test_not_onepass!(onepass_not_in_rust, r"(\d+)-(\d+)");
+
+    test_onepass!(onepass_empty_alt_1_, r"(a|())b");
+    test_not_onepass!(onepass_empty_alt_2_, r"(a|())a");
+
+    test_not_onepass!(onepass_rep_1_, r"a*a");
+    test_not_onepass!(onepass_rep_2_, r"a+a");
+    test_not_onepass!(onepass_rep_3_, r"a{4,8}a");
+    test_not_onepass!(onepass_rep_4_, r"a{4,}a");
+    test_onepass!(onepass_rep_5_, r"a{4}a");
+    test_not_onepass!(onepass_rep_6_, r"a?a");
+
+    test_onepass!(onepass_rep_7_, r"a*b");
+    test_onepass!(onepass_rep_8_, r"a+b");
+    test_onepass!(onepass_rep_9_, r"a{4,8}b");
+    test_onepass!(onepass_rep_10_, r"a{4,}b");
+    test_onepass!(onepass_rep_11_, r"a{4}b");
+    test_onepass!(onepass_rep_12_, r"a?b");
+
+    test_not_onepass!(onepass_concat_middle_1_, r"ab?bc");
+    test_onepass!(onepass_concat_middle_2_, r"a(?:b|c)dc");
+
+    test_not_onepass!(onepass_unicode_class_1_, r"\d");
+    test_not_onepass!(onepass_unicode_class_2_, r"\s");
+    test_not_onepass!(onepass_unicode_class_3_, r"\w");
+    test_not_onepass!(onepass_unicode_class_4_, r"inthe\wmiddle");
+
+    test_not_onepass!(onepass_unicode_clash_1_, r"Δ|δ");
+
+    test_not_onepass!(onepass_empty_assert_1_, r"a|^a");
+    test_onepass!(onepass_empty_assert_2_, r"\ba");
+    test_onepass!(onepass_empty_assert_3_, r"^a");
+    test_onepass!(onepass_empty_assert_4_, r"a$");
+
+    test_not_onepass!(onepass_naked_empty_assert_1_, r"\w|a");
 }
