e0ee608121df4c100e16ba1e1dd57be27de70c0b
davkor <david@adalogics.com>
2020-05-14 20:56:17 +0100

    Added fuzzing infrastructure for OSS-Fuzz. Fuzzer similar to cargo-fuzz targets.

---
+++ fuzz/fuzz_targets/fuzz_regex_match.rs
@@ -0,0 +1,19 @@
+#![no_main]
+use libfuzzer_sys::fuzz_target;
+
+fuzz_target!(|data: &[u8]| {
+    if let Ok(data) = std::str::from_utf8(data) {
+        // split data into regular expression and actual input to search through
+        use std::cmp::max;
+        let len = data.chars().count();
+        let split_off_point = max(len / 5, 1) as usize;
+        let char_index = data.char_indices().nth(split_off_point);
+
+        if let Some((char_index, _)) = char_index {
+            let (pattern, input) = data.split_at(char_index);
+            if let Ok(re) = regex::Regex::new(pattern) {
+                re.is_match(input);
+            }
+        }
+    }
+});
