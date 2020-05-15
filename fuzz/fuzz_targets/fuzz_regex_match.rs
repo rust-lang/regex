91b7b6d712f0671eda2580ee0c93b4c32b083d05
davkor <david@adalogics.com>
2020-05-15 22:21:57 +0100

    Updated the fuzzer so input and regex are more independent.

--- fuzz/fuzz_targets/fuzz_regex_match.rs
+++ fuzz/fuzz_targets/fuzz_regex_match.rs
@@ -2,13 +2,16 @@
 use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|data: &[u8]| {
-    if let Ok(data) = std::str::from_utf8(data) {
-        // split data into regular expression and actual input to search through
+    if data.len() < 2 {
+        return;
+    }
+    let split_point = data[0] as usize;
+    if let Ok(data) = std::str::from_utf8(&data[1..]) {
         use std::cmp::max;
+        // split data into regular expression and actual input to search through
         let len = data.chars().count();
-        let split_off_point = max(len / 5, 1) as usize;
+        let split_off_point = max(split_point, 1) % len as usize;
         let char_index = data.char_indices().nth(split_off_point);
-
         if let Some((char_index, _)) = char_index {
             let (pattern, input) = data.split_at(char_index);
             if let Ok(re) = regex::Regex::new(pattern) {
