8773579baf55bc801c32d5e4c20293baf75d325e
Addison Crump <me@addisoncrump.info>
2022-03-08 16:35:13 -0600

    fuzz: add syntactic structurally aware fuzzers
    This makes uses of the new 'arbitrary' feature in 'regex-syntax' to make
    fuzzing much more targeted and complete.
    
    Closes #848

---
+++ fuzz/fuzz_targets/ast_fuzz_match.rs
@@ -0,0 +1,30 @@
+#![no_main]
+
+use {
+    libfuzzer_sys::fuzz_target, regex::RegexBuilder, regex_syntax::ast::Ast,
+};
+
+#[derive(Eq, PartialEq, arbitrary::Arbitrary)]
+struct FuzzData {
+    ast: Ast,
+    haystack: String,
+}
+
+impl std::fmt::Debug for FuzzData {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut builder = f.debug_struct("FuzzData");
+        builder.field("ast", &format!("{}", self.ast));
+        builder.field("haystack", &self.haystack);
+        builder.finish()
+    }
+}
+
+fuzz_target!(|data: FuzzData| {
+    let pattern = format!("{}", data.ast);
+    let Ok(re) = RegexBuilder::new(&pattern).size_limit(1<<20).build() else {
+        return
+    };
+    re.is_match(&data.haystack);
+    re.find(&data.haystack);
+    re.captures(&data.haystack).map_or(0, |c| c.len());
+});
