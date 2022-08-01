d175482eff8514034378b8f27d78b765f7f743e6
snsmac <snsmac@users.noreply.github.com>
2022-08-01 20:38:40 +0200

    Syntax: Make \p{Sc} work
    'sc' refers to the 'Currency_Symbol' general category,
    but is also the abbreviation for the 'Script' property.
    
    Fixes #835
    Related #719 b1489c8

--- regex-syntax/src/unicode.rs
+++ regex-syntax/src/unicode.rs
@@ -243,7 +243,9 @@ impl<'a> ClassQuery<'a> {
         // a general category. (Currently, we don't even support the
         // 'Case_Folding' property. But if we do in the future, users will be
         // required to spell it out.)
-        if norm != "cf" {
+        // 'sc' refers to the 'Currency_Symbol' general category, but is also
+        // the abbreviation for the 'Script' property.
+        if norm != "cf" && norm != "sc" {
             if let Some(canon) = canonical_prop(&norm)? {
                 return Ok(CanonicalClassQuery::Binary(canon));
             }
