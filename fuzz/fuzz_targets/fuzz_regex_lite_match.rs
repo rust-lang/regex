e6c8b96265770bf1cda35fb3fae0adfe84968be8
5225225 <5225225@mailbox.org>
2023-05-04 15:21:05 +0100

    fuzz: use structured fuzzer input
    This makes a couple of the fuzzer targets a bit nicer by just asking for
    structured data instead of trying to manifest it ourselves out of a
    &[u8].
    
    Closes #821

--- fuzz/fuzz_targets/fuzz_regex_lite_match.rs
+++ fuzz/fuzz_targets/fuzz_regex_lite_match.rs
@@ -1,23 +1,60 @@
 #![no_main]
 
-use libfuzzer_sys::fuzz_target;
+use libfuzzer_sys::{arbitrary, fuzz_target};
 
-fuzz_target!(|data: &[u8]| {
-    let _ = run(data);
-});
-
-fn run(data: &[u8]) -> Option<()> {
-    if data.len() < 2 {
-        return None;
-    }
-    let mut split_at = usize::from(data[0]);
-    let data = std::str::from_utf8(&data[1..]).ok()?;
-    // Split data into a regex and haystack to search.
-    let len = usize::try_from(data.chars().count()).ok()?;
-    split_at = std::cmp::max(split_at, 1) % len;
-    let char_index = data.char_indices().nth(split_at)?.0;
-    let (pattern, input) = data.split_at(char_index);
-    let re = regex_lite::Regex::new(pattern).ok()?;
-    re.is_match(input);
-    Some(())
+#[derive(arbitrary::Arbitrary)]
+struct FuzzCase<'a> {
+    pattern: &'a str,
+    haystack: &'a str,
+    case_insensitive: bool,
+    multi_line: bool,
+    crlf: bool,
+    dot_matches_new_line: bool,
+    swap_greed: bool,
+    ignore_whitespace: bool,
 }
+
+impl std::fmt::Debug for FuzzCase<'_> {
+    fn fmt(
+        &self,
+        fmt: &mut std::fmt::Formatter,
+    ) -> Result<(), std::fmt::Error> {
+        let FuzzCase {
+            pattern,
+            case_insensitive,
+            multi_line,
+            crlf,
+            dot_matches_new_line,
+            swap_greed,
+            ignore_whitespace,
+            haystack,
+        } = self;
+
+        write!(
+            fmt,
+            r#"
+let Ok(re) = regex_lite::RegexBuilder::new({pattern:?})
+    .case_insensitive({case_insensitive:?})
+    .multi_line({multi_line:?})
+    .crlf({crlf:?})
+    .dot_matches_new_line({dot_matches_new_line:?})
+    .swap_greed({swap_greed:?})
+    .ignore_whitespace({ignore_whitespace:?})
+    .build() else {{ return }};
+re.is_match({haystack:?});
+        "#
+        )
+    }
+}
+
+fuzz_target!(|case: FuzzCase| {
+    let Ok(re) = regex_lite::RegexBuilder::new(case.pattern)
+        .case_insensitive(case.case_insensitive)
+        .multi_line(case.multi_line)
+        .crlf(case.crlf)
+        .dot_matches_new_line(case.dot_matches_new_line)
+        .swap_greed(case.swap_greed)
+        .ignore_whitespace(case.ignore_whitespace)
+        .build() else { return };
+    re.is_match(case.haystack);
+});
