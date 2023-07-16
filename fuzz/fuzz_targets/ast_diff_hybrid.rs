312764d055e63e1e33eb591ba8cfb858a36a12ec
Addison Crump <addison.crump@cispa.de>
2023-07-16 22:37:53 +0200

    panic safety

--- fuzz/fuzz_targets/ast_diff_hybrid.rs
+++ fuzz/fuzz_targets/ast_diff_hybrid.rs
@@ -1,5 +1,6 @@
 #![no_main]
 
+use regex_automata::Input;
 use {
     libfuzzer_sys::{fuzz_target, Corpus},
     regex_automata::{
@@ -43,16 +44,20 @@ fn do_fuzz(data: FuzzData) -> Corpus {
     };
     let mut hybrid_cache = re.create_cache();
 
-    assert_eq!(
-        re.is_match(&mut hybrid_cache, &data.haystack),
-        baseline.is_match(&mut cache, &data.haystack)
-    );
-    let found1 = re.find(&mut hybrid_cache, &data.haystack);
-    let found2 = baseline.find(&mut cache, &data.haystack);
-    if let Some(found1) = found1 {
-        let found2 = found2.expect("Found in target, but not in baseline!");
-        assert_eq!(found1.start(), found2.start());
-        assert_eq!(found1.end(), found2.end());
+    if let Ok(maybe_match) =
+        re.try_search(&mut hybrid_cache, &Input::new(&pattern))
+    {
+        assert_eq!(
+            maybe_match.is_some(),
+            baseline.is_match(&mut cache, &data.haystack)
+        );
+        let found2 = baseline.find(&mut cache, &data.haystack);
+        if let Some(found1) = maybe_match {
+            let found2 =
+                found2.expect("Found in target, but not in baseline!");
+            assert_eq!(found1.start(), found2.start());
+            assert_eq!(found1.end(), found2.end());
+        }
     }
 
     // no captures
