f4432484ef290208803a6c5b52eab9cad46d5266
Addison Crump <addison.crump@cispa.de>
2023-07-16 21:51:37 +0200

    add diff_nfas, function out do_fuzz so clippy can do its thing

--- fuzz/oss-fuzz-build.sh
+++ fuzz/oss-fuzz-build.sh
@@ -3,17 +3,7 @@
 cd $SRC/regex
 cargo fuzz build -O --debug-assertions
 
-targets=(
-  fuzz_regex_match
-  fuzz_regex_lite_match
-  fuzz_regex_automata_deserialize_dense_dfa
-  fuzz_regex_automata_deserialize_sparse_dfa
-  ast_roundtrip
-  ast_fuzz_match
-  ast_fuzz_regex
-  ast_fuzz_match_bytes
-  ast_diff_default
-)
+targets=$(cargo fuzz list)
 for target in "${targets[@]}"; do
   cp "fuzz/target/x86_64-unknown-linux-gnu/release/${target}" "${OUT}/"
   if [[ "$target" == ast_* ]]; then
