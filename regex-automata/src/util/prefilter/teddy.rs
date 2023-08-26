fb0a8878c91ef4b0eb946705c9136e478bef4209
Andrew Gallant <jamslam@gmail.com>
2023-08-26 08:48:26 -0400

    automata: fix incorrect use of Aho-Corasick's "standard" semantics
    This fixes a bug in how prefilters were applied for multi-regexes
    compiled with "all" semantics. It turns out that this corresponds to the
    regex crate's RegexSet API, but only its `is_match` routine.
    
    See the comment on the regression test added in this PR for an
    explanation of what happened. Basically, it came down to incorrectly
    using Aho-Corasick's "standard" semantics, which doesn't necessarily
    report leftmost matches. Since the regex crate is really all about
    leftmost matching, this can lead to skipping over parts of the haystack
    and thus lead to missing matches.
    
    Fixes #1070

--- regex-automata/src/util/prefilter/teddy.rs
+++ regex-automata/src/util/prefilter/teddy.rs
@@ -50,12 +50,17 @@ impl Teddy {
             // theory we could at least support leftmost-longest, as the
             // aho-corasick crate does, but regex-automata doesn't know about
             // leftmost-longest currently.
+            //
+            // And like the aho-corasick prefilter, if we're using `All`
+            // semantics, then we can still use leftmost semantics for a
+            // prefilter. (This might be a suspicious choice for the literal
+            // engine, which uses a prefilter as a regex engine directly, but
+            // that only happens when using leftmost-first semantics.)
             let (packed_match_kind, ac_match_kind) = match kind {
-                MatchKind::LeftmostFirst => (
+                MatchKind::LeftmostFirst | MatchKind::All => (
                     aho_corasick::packed::MatchKind::LeftmostFirst,
                     aho_corasick::MatchKind::LeftmostFirst,
                 ),
-                _ => return None,
             };
             let minimum_len =
                 needles.iter().map(|n| n.as_ref().len()).min().unwrap_or(0);
