20bc5989ad80961199c29ab40ac0ee796ebaa1e4
chris-ha458 <hac541309@gmail.com>
2023-11-22 21:56:20 +0900

    lint: fix a selection of Clippy lints
    PR #1129

--- regex-automata/src/nfa/thompson/compiler.rs
+++ regex-automata/src/nfa/thompson/compiler.rs
@@ -1665,7 +1665,7 @@ impl Compiler {
         capture_index: u32,
         name: Option<&str>,
     ) -> Result<StateID, BuildError> {
-        let name = name.map(|n| Arc::from(n));
+        let name = name.map(Arc::from);
         self.builder.borrow_mut().add_capture_start(
             StateID::ZERO,
             capture_index,
