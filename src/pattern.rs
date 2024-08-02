e79febe47cf1e47c11b901a9b476749b8668e2fc
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2024-08-02 14:35:43 +0200

    Rust nightly removed the lifetime from Pattern

--- src/pattern.rs
+++ src/pattern.rs
@@ -10,10 +10,10 @@ pub struct RegexSearcher<'r, 't> {
     next_match: Option<(usize, usize)>,
 }
 
-impl<'r, 't> Pattern<'t> for &'r Regex {
-    type Searcher = RegexSearcher<'r, 't>;
+impl<'r> Pattern for &'r Regex {
+    type Searcher<'t> = RegexSearcher<'r, 't>;
 
-    fn into_searcher(self, haystack: &'t str) -> RegexSearcher<'r, 't> {
+    fn into_searcher(self, haystack: &str) -> RegexSearcher<'r, '_> {
         RegexSearcher {
             haystack,
             it: self.find_iter(haystack),
