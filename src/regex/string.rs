0fc52dca5df749ca698b0a33c776b8654de2190b
Thayne McCombs <astrothayne@gmail.com>
2024-09-19 01:41:40 -0600

    api: Add as_match method
    To simplify getting the whole match from Captures

--- src/regex/string.rs
+++ src/regex/string.rs
@@ -1675,6 +1675,15 @@ impl<'h> Captures<'h> {
             .map(|sp| Match::new(self.haystack, sp.start, sp.end))
     }
 
+    /// Return the overall match for the capture.
+    ///
+    /// This returns the match for index `0`. That is it is equivalent to
+    /// `get(0).unwrap()`
+    #[inline]
+    pub fn as_match(&self) -> Match {
+        self.get(0).unwrap()
+    }
+
     /// Returns the `Match` associated with the capture group named `name`. If
     /// `name` isn't a valid capture group or it refers to a group that didn't
     /// match, then `None` is returned.
