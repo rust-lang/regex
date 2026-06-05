deda7a3325381bdb867d1a793f1ef5cb9fddfa19
marko1olo <barsukdana@gmail.com>
2026-06-05 22:30:23 +0400

    docs: clarify unsupported anchored pattern searches

--- regex-automata/src/util/search.rs
+++ regex-automata/src/util/search.rs
@@ -1879,6 +1879,12 @@ pub enum MatchErrorKind {
     /// An error indicating that a particular type of anchored search was
     /// requested, but that the regex engine does not support it.
     ///
+    /// When this error occurs with [`Anchored::Pattern`], then one common
+    /// cause is that the underlying regex engine was built without support
+    /// for anchored starting states for each pattern. For DFA engines, this
+    /// typically means enabling the `starts_for_each_pattern` configuration
+    /// option when building the searcher.
+    ///
     /// Note that this error should not be returned by a regex engine simply
     /// because the pattern ID is invalid (i.e., equal to or exceeds the number
     /// of patterns in the regex). In that case, the regex engine should report
